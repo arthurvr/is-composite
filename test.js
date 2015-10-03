cb96c8b89ae45cd82834d21550566e19aa35e3c2
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 12:17:25 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,21 +1,20 @@
-'use strict';
-var test = require('ava');
-var isComposite = require('./');
+import test from 'ava';
+import isComposite from './';
 
-test('should check if a number is composite', function (t) {
-	[1, -1, 0, 2, 3, 997, 7919, 999983, 4.1, 122.001].forEach(function (number) {
+test('should check if a number is composite', t => {
+	[1, -1, 0, 2, 3, 997, 7919, 999983, 4.1, 122.001].forEach(number => {
 		t.false(isComposite(number));
 	});
 
-	[4, 77, 122, 999999].forEach(function (number) {
+	[4, 77, 122, 999999].forEach(number => {
 		t.true(isComposite(number));
 	});
 
 	t.end();
 });
 
-test('should throw when not passing a number', function (t) {
-	t.throws(function () {
+test('should throw when not passing a number', t => {
+	t.throws(() => {
 		isComposite({});
 	}, TypeError);
 
